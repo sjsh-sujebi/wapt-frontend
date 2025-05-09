876813bbf7746f7366fda06530fff710d3ddd02d
unknown <dolphin2410@outlook.kr>
2025-05-09 23:44:01 +0900

    verify tamper

--- src/components/Printer.tsx
+++ src/components/Printer.tsx
@@ -18,8 +18,17 @@ export default function HomePage() {
                 headers: {
                     "Content-Type": "application/json"
                 },
-            }).then((data) => {
+            }).then(async (data) => {
                 const response = data.data as APIResponse
+
+                const verify_response = (await axios.post("/.netlify/functions/file_verify", JSON.stringify({
+                    base64File: response.payload.base64Data,
+                    code: code
+                }))).data as APIResponse
+
+                if (!verify_response.is_success) {
+                    alert("File Tampered!")
+                }
                 
                 const buffer = Buffer.from(response.payload.base64Data.split(`${response.payload.contentType}base64`)[1], 'base64')
                 const blob = new Blob([buffer], { type : response.payload.contentType })

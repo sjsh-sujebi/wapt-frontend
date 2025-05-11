b28b87e80a771f02367a979179166a9593a04ea1
unknown <dolphin2410@outlook.kr>
2025-05-11 15:17:01 +0900

    better colors

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -52,6 +52,7 @@ export default function MainPage() {
                         <span className="upfont">USB</span>나 소셜 계정을 사용하지 않고도 프린터에 파일을 간편하게 전송할 수 있도록 해주는 서비스입니다<span className="upfont">. USB</span>가 없을 때 출력을 하지 못하는 불상사를 막기 위해서<span className="upfont">,</span> 그리고 프린터 컴퓨터에서 소셜 계정 로그인 시의 보안 문제를 해결하기 위해 제작되었습니다<span className="upfont">.</span>
                     </div>
                     <br />
+                    <br />
                 </div>
                 <h1 className="mp_explanation_topic_title upfont">Features</h1>
                 <div className="htu_container">
@@ -60,6 +61,7 @@ export default function MainPage() {
                         브라우저와 인터넷만 있다면 별도 설치 과정 없이 언제 어디서든지 사용 가능합니다<span className="upfont">!</span>
                     </div>
                     <br />
+                    <br />
                 </div>
                 <div className="htu_container">
                     <h2 id="realtime" onClick={() => clip("realtime")}># <span className="upfont">Realtime</span></h2>
@@ -67,12 +69,14 @@ export default function MainPage() {
                         <span className="upfont">Google Firebase API</span>를 활용하여 실시간 파일 공유를 구현하였습니다<span className="upfont">. P2P</span> 통신 방식보다 더욱 안정적인 파일 전송을 진행할 수 있습니다<span className="upfont">.</span>
                     </div>
                     <br />
+                    <br />
                 </div>
                 <div className="htu_container">
                     <h2 id="blockchain" onClick={() => clip("blockchain")}># <span className="upfont">Blockchain</span></h2>
                     <div className="mdownfont">
                         파일이 전송 과정에서 손상되거나 일부 손상되지 않았는지 확인하기 위해 블록체인 기술을 활용하여 데이터 무결성을 보장합니다<span className="upfont">.</span> 이를 무시하고 빠른 공유를 원한다면 해당 기능을 비활성화할 수 있습니다<span className="upfont">.</span>
                     </div>
+                    <br />
                     <br />
                 </div>
                 <h1 className="mp_explanation_topic_title upfont">Open Source</h1>
@@ -85,6 +89,7 @@ export default function MainPage() {
                         <a href="https://www.buymeacoffee.com/dolphin2410" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/arial-blue.png" alt="Buy Me A Coffee" style={{height: '30px', width: '110px'}} /></a>
                         <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="dolphin2410" data-color="#5F7FFF" data-emoji="" data-font="Arial" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#ffffff" data-coffee-color="#FFDD00" ></script>
                     </div>
+                    <br />
                     <br />
                 </div>
             </div>

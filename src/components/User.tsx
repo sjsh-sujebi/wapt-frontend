0c239f642a9380981746cd837f5802b11ddba76e
unknown <dolphin2410@outlook.kr>
2025-05-19 22:03:43 +0900

    reset the button

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -112,7 +112,8 @@ function sign_transaction(set_success: (msg: string) => void, myHash: string, se
                     alert("BlobUploadError: 오류가 발생했습니다. 개발자에게 문의해주세요.")
                 }
             }).catch(e => {
-                alert("파일이 너무 큰 건 아닌가요?")
+                document.querySelector("#file_selection_btn")?.classList.remove("us_submit_deactivated")
+                alert("실패! 파일이 너무 큰 건 아닌가요?")
             })
         }
     })

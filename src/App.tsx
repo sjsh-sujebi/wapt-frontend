a541284eea2c1da12061072dcb83e410ac8710bc
dolphin2410 <80996692+dolphin2410@users.noreply.github.com>
2025-05-22 00:44:02 +0900

    Adfit

--- src/App.tsx
+++ src/App.tsx
@@ -37,6 +37,11 @@ function App({child}: {child: JSX.Element}) {
     <div className="App">
       <TopNav hash={hash} toggleMyPage={() => window.location.href = "/mypage"} toggleHome={() => window.location.href = "/"} toggleMenu={() => {}} toggleLogin={() => window.location.href = "/login"} />
       {child}
+      <ins className="kakao_ad_area" style={{display: "none"}}
+data-ad-unit = "DAN-j1XnRWcT8AtchtNp"
+data-ad-width = "320"
+data-ad-height = "100"></ins>
+<script type="text/javascript" src="//t1.daumcdn.net/kas/static/ba.min.js" async></script>
       <BottomFooter />
     </div>
   );
